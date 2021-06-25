80a8f82175755ff3d0d345d5f510d270f6bbbc29
Giovanni Branco <giovannipbranco1@gmail.com>
2021-06-25 19:06:14 -0300

    update deploy

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, "build")));
+
++app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(process.env.PORT || 3030),
+  (error) => {
+    if (erro) {
+      return console.log(error);
+    }
+  };
+
+const server = app.listen(app.get("port"), function () {
+  console.log("listening on port ", server.address().port);
+});
